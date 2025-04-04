73ad393e3296b0be22c7c4bc5fe782a03a596f74
BiG A <antoine.salameh.45@gmail.com>
2025-04-04 15:34:09 +0300

    custom styled searchbar to search user

---
+++ src/components/molecules/SearchUser.tsx
@@ -0,0 +1,13 @@
+const SearchUser = () => {
+  return (
+    <>
+        <input
+          type="text"
+          placeholder="Search users..."
+          className="px-3 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400"
+        />
+    </>
+  )
+}
+
+export default SearchUser
