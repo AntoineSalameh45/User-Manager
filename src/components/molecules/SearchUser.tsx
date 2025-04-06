578a818c0e84f5f1a49b472595520dca75f66761
BiG A <antoine.salameh.45@gmail.com>
2025-04-06 12:54:41 +0300

    added an id in case of a warning

--- src/components/molecules/SearchUser.tsx
+++ src/components/molecules/SearchUser.tsx
@@ -5,6 +5,7 @@ const SearchUser = () => {
           type="text"
           placeholder="Search users..."
           className="px-3 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400"
+          id="searchuser"
         />
     </>
   )
