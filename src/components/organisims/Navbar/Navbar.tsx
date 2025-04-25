d4f12c70e6501d0c3362323d1b5f874f98ddc5dd
BiG A <antoine.salameh.45@gmail.com>
2025-04-25 12:33:59 +0300

    feat: wrap User Management title in Link for improved navigation

--- src/components/organisims/Navbar/Navbar.tsx
+++ src/components/organisims/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router";
+import { Link, useNavigate } from "react-router";
 import { CustomButton } from "../../atoms/CustomButton";
 import { ThemeButton } from "../../atoms/ThemeButton";
 import useSessionStore from "../../../store/authStore";
@@ -14,13 +14,16 @@ const Navbar = () => {
 
   return (
     <div className="flex justify-between items-center bg-primary shadow-lg w-full p-4 transition-all">
-      <h2 className="text-white text-xl select-none">User Management</h2>
+      <Link to="/dashboard">
+        <h2 className="text-white text-xl select-none">User Management</h2>
+      </Link>
       <div className="flex items-center gap-2 text-sm">
         <CustomButton
           text="Create User"
           bgColor="bg-white"
           textColor="text-primary"
           hoverBgColor="hover:bg-[#ffffffdd]"
+          onClick={() => navigate("/dashboard/new")}
         />
         <CustomButton
           text="Logout"
